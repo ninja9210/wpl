6ca23170b5a9c93f6aabe52b9da9088a2e74682a
ninja9210 <pp506345@outlook.com>
2024-11-26 19:01:23 +0530

    Create pract7.php

---
+++ pract7.php
@@ -0,0 +1,18 @@
+Practical No 7: Design a page web using PHP to demonstrate, variables, echo/print, data types,string 
+functions and operators 
+
+<!DOCTYPE html> 
+<html> 
+<body> 
+<?php 
+$txt = "Hello world!"; 
+$x = 5; $y = 10.5; 
+echo $txt; 
+echo "<br>"; 
+echo 
+$x; echo 
+"<br>"; echo 
+$y; 
+?> 
+</body> 
+</html>
